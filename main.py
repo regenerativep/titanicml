c1c7f20f5e5190221ecec9997c933b1b46bf972e
Regen <30307062+regenerativep@users.noreply.github.com>
2019-10-08 10:32:45 -0400

    loading data

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 import pandas as pd
+column_names = ["PassengerId", "Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"]
+training_data = pd.read_csv("train.csv", names=column_names)
+print(training_data)
