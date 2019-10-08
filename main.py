db5b12eada02f0fcb2a6a163d20c41dd996cdf1c
ambientaardvark <42986610+ambientaardvark@users.noreply.github.com>
2019-10-08 11:04:16 -0400

    detele name, ticket num

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import pandas as pd
 column_names = ["PassengerId", "Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"]
 training_data = pd.read_csv("train.csv", names=column_names)
 training_data = training_data.drop(training_data.index[0])
+training_data = training_data.drop('Name', axis = 1)
+training_data = training_data.drop('Ticket', axis = 1)
 print(training_data)
