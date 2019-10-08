07923eeffa67c448ad8e4f09890145b8a796955f
Regen <30307062+regenerativep@users.noreply.github.com>
2019-10-08 10:45:32 -0400

    a

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
 column_names = ["PassengerId", "Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"]
 training_data = pd.read_csv("train.csv", names=column_names)
+training_data = training_data.drop(training_data.index[0])
 print(training_data)
