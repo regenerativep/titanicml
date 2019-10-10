0fab3987c1b0d758bf76a098f2627841fab9e28e
nathandorson <nathandorson@gmail.com>
2019-10-10 09:58:48 -0400

    Update siblingsPP.py

--- siblingsPP.py
+++ siblingsPP.py
@@ -8,6 +8,6 @@ def siblingsPP(training_data):
             sibSp.append(-1)
             sibSpExists.append(0)
         else:
-            sibSp.append(age)
+            sibSp.append(sibs)
             sibSpExists.append(1)
     return [sibSp, sibSpExists]
