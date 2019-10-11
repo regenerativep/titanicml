169e0e80e19e1b5b85100e1ebe213fcb2a903703
Ykechriotis <56162172+Ykechriotis@users.noreply.github.com>
2019-10-11 09:19:33 -0400

    Update ParchFarePP.py

--- ParchFarePP.py
+++ ParchFarePP.py
@@ -1,4 +1,25 @@
-def ParchFarePPpy(Parch, Fare):
-    arr = [Parch, Fare]
-    return arr
-print(ParchFarePPpy(5,10))
+import pandas as pd
+
+def ParchPP(training_data):
+    parches = []
+    parchExists = []
+    for parch in training_data.get('Parch'):
+        if str(parch) == 'nan':
+            parches.append(-1)
+            parchExists.append(0)
+        else:
+            parches.append(parch)
+            parchExists.append(1)
+    return [parches, parchExists]
+
+def FarePP(training_data):
+    fares = []
+    fareExists = []
+    for fare in training_data.get('Fare'):
+        if str(fare)=='nan':
+            fares.append(-1)
+            fareExists.append(0)
+        else:
+            fares.append(fare)
+            fareExists.append(1)
+    return[fares,fareExists]
