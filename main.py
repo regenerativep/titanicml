2853a585a9346d33c3c41262719ec0ac2a08d965
Regen <30307062+regenerativep@users.noreply.github.com>
2019-10-17 16:31:10 -0400

    successful preprocessing -> forest model -> results

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import math
+import numpy as np
 import preprocessing as pp
+from sklearn.ensemble import RandomForestClassifier
 column_names = ["PassengerId", "Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"]
 # passenger id is number irrelavent to people of the titanic (probably dont use in model)
 # survived is if the given person survived
@@ -10,11 +12,29 @@ training_data = pd.read_csv("train.csv", names=column_names)
 training_data = training_data.drop(training_data.index[0])
 # training_data = training_data.drop('Name', axis = 1)
 # training_data = training_data.drop('Ticket', axis = 1)
+survived = training_data["Survived"]
 training_data = training_data.drop('Survived',axis = 1)
 #print(training_data)
+def predict(training_data, training_survived):
+    test_data = pd.read_csv("test.csv", names=['PassengerId','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked'])
+    test_data = test_data.drop(test_data.index[0])
+    processed_test_data = pp.preprocess(test_data)
+    processed_test_data_frame = pp.arrayRowsToDataframe(processed_test_data)
 
-print(pp.preprocess(training_data))
+    model = RandomForestClassifier(n_estimators=1000, max_depth=10, random_state=1)
+    model.fit(training_data, training_survived)
+    predictions = model.predict(processed_test_data_frame)
 
-import kagglelearn
+    output = pd.DataFrame({"PassengerId": test_data.PassengerId, "Survived": predictions})
+    output.to_csv("test_prediction.csv", index=False)
+    print("saved predictions")
+
+
+dataRows = pp.preprocess(training_data)
+dataFrame = pp.arrayRowsToDataframe(dataRows)
+print(dataFrame)
+predict(dataFrame, survived)
+
+# import kagglelearn
 # kagglelearn.predict(training_data)
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,24 @@
 import math
 import pandas as pd
+import numpy as np
+import csv
+import collections
+
+def arrayRowsToDataframe(arr):
+    np.savetxt("temp.csv", arr, delimiter=",", fmt="%s")
+    columnNames = []
+    for i in range(len(arr[0])):
+        columnNames.append(str(i))
+    return pd.read_csv("temp.csv", names=columnNames)
+    
+
+def preprocess(inp):
+    data = inp.iloc
+    processed_data = []
+    for i in range(inp.shape[0]):
+        processed_data.append(format_row(data[i]))
+    #print(processed_data)
+    return processed_data
 
 def format_ages(age):
     age = float(age)
@@ -9,21 +28,13 @@ def format_ages(age):
     else:
         return [-1, 0]
 
-
-def preprocess(inp):
-    data = inp.iloc
-    processed_data = []
-    for i in range(inp.shape[0]):
-        processed_data.append(format_row(data[i]))
-    return processed_data
-
 #function that takes in a row in array form and turns it into another array
 #'ID','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked'
 def format_row(row):
     pprow = []
-    pprow += format_ticket_class(row[1])
-    pprow += format_sex(row[3])
-    pprow += format_ages(row[4])
+    pprow += format_ticket_class(row[1]) #0, 1, 2
+    pprow += format_sex(row[3]) #3, 4
+    pprow += format_ages(row[4]) #5, 6
     pprow += format_sibs(row[5])
     pprow += format_parch(row[6])
     pprow += format_fare(row[8])
@@ -65,11 +76,9 @@ def format_sibs(sibs):
 # take in val of (nan, 1, 2, 3). outputs array of [is first ? 1 : 0, is second ? 1 : 0, is third ? 1 : 0]
 def format_ticket_class(val):
     arr = [0, 0, 0]
-    if not isinstance(val, int):
-        return arr
-    for i in range(1, 4, 1):
-        if val == i:
-            arr[i - 1] = 1
+    for i in range(3):
+        if i + 1 == int(val):
+            arr[i] = 1
     return arr
 
 def format_sex(val):
