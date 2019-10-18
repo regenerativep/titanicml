65a3094316348b7013bbfa6e84f162f323be4ebc
ambientaardvark <42986610+ambientaardvark@users.noreply.github.com>
2019-10-18 09:41:37 -0400

    updated comparefiles

--- compareFiles.py
+++ compareFiles.py
@@ -4,9 +4,10 @@ def compare(file1, file2):
     survived1 = pd.read_csv(file1)['Survived']
     survived2 = pd.read_csv(file2)['Survived']
     differences = 0
-    for i in [0, len(survived1) - 1]:
+    for i in range(len(survived1) - 1):
         if not survived1[i] == survived2[i]:
             differences += 1
+        i += 1
     return differences
 
-print(compare('titantic_prediction.csv', 'titantic_prediction.csv'))
+print(compare('titantic_prediction.csv', 'test_prediction.csv'))
