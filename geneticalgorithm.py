fd4246f74da7c4d62bd4947fdeef2c649d653584
Regen <30307062+regenerativep@users.noreply.github.com>
2019-10-24 09:47:58 -0400

    improved ga

--- geneticalgorithm.py
+++ geneticalgorithm.py
@@ -1,5 +1,7 @@
 import random
 import math
+import neural_net as nn
+import preprocessing as pp
 def run_generation(organism, num_children):
     children = [organism]
     for i in range(num_children):
@@ -35,13 +37,14 @@ class NeuralOrganism:
     def __init__(self, model):
         self.model = model
     def getScore(self):
-        results = self.model.predict(inputs)
+        
+        results = self.model.calculate_output(inputs)
         cost = getCost(desiredOutputs, results)
         return cost
-    #def mutate(self):
-        #copy nn
-        #mutate it
-        #return it
+    def mutate(self):
+        newOrg = nn.NeuralNet(None, self.model)
+        newOrg.mutate()
+        return newOrg
 def copyArray(arr):
     newArr = []
     for item in arr:
@@ -63,20 +66,20 @@ class TestOrganism:
                 y -= 1
             elif item == "V":
                 y += 1
-        targetX = 32
+        targetX = 128
         targetY = 32
         return (targetX - x) ** 2 + (targetY - y) ** 2 + len(self.code) * 10
     def mutate(self):
         newOrg = TestOrganism(copyArray(self.code))
-        codeLen = len(self.code)
-        for i in range(len(self.code), -1, -1):
-            if i != len(self.code) and len(self.code) > 0:
-                if random.random() < 2 / codeLen:
-                    self.code[i] = random.choice(["<", ">", "^", "V"])
-                if random.random() < 2 / codeLen:
-                    del self.code[i]
-            if codeLen == 0 or random.random() < 2 / codeLen:
-                self.code = self.code[:i] + [random.choice(["<", ">", "^", "V"])] + self.code[i:]
+        codeLen = len(newOrg.code)
+        for i in range(len(newOrg.code), -1, -1):
+            if i != len(newOrg.code) and len(newOrg.code) > 0:
+                if random.random() < 1 / codeLen:
+                    newOrg.code[i] = random.choice(["<", ">", "^", "V"])
+                if random.random() < 1 / codeLen:
+                    del newOrg.code[i]
+            if codeLen == 0 or random.random() < 1 / codeLen:
+                newOrg.code = newOrg.code[:i] + [random.choice(["<", ">", "^", "V"])] + newOrg.code[i:]
         return newOrg
 def codeToStr(code):
     codeStr = ""
@@ -95,13 +98,12 @@ if __name__ == "__main__":
     org = TestOrganism([])
     lastCode = org.code
     gensWithoutChange = 0
-    for i in range(10000):
-        childrenCount = 50 + gensWithoutChange * 50
+    for i in range(1000):
+        childrenCount = 20 + gensWithoutChange * 20
         org = run_generation(org, childrenCount)
         if not arrEquals(lastCode, org.code):
-            #arrEquals(lastCode, org.code)
             gensWithoutChange = 0
         else:
             gensWithoutChange += 1
-        print(str(i) + "," + str(childrenCount) + ":" + str(org.getScore()) + ", " + str(len(org.code)) + "," + codeToStr(org.code))
+        print(str(i) + ", " + str(childrenCount) + ", score: " + str(org.getScore()))# + ", " + str(len(org.code)) + "," + codeToStr(org.code))
         lastCode = org.code
